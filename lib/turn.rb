14deec47c4de64e826ad92bf415e6b6accce09ac
Michael Ciabuca <ciabuca@gmail.com>
2020-06-29 21:51:21 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+board = [" "," "," "," "," "," "," "," "," "];
+
+def display_board(boards)
+
+  puts " #{boards[0]} | #{boards[1]} | #{boards[2]} ";
+  puts "-----------"
+  puts " #{boards[3]} | #{boards[4]} | #{boards[5]} ";
+  puts "-----------"
+  puts " #{boards[6]} | #{boards[7]} | #{boards[8]} ";
+
+end
+
+display_board(board);
